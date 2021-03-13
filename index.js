10a836cb424cac5c584e99a2930a8eb113cfd889
svetlana stojanovic <svetlana.stojanovic.86@gmail.com>
2021-03-13 11:47:58 +0100

    redirecting to the thank_you_page, still having an ECONNREFUSED error, removed cookies trial for now

--- index.js
+++ index.js
@@ -10,10 +10,19 @@ const cookieSession = require("cookie-session");
 app.engine("handlebars", hb());
 app.set("view engine", "handlebars");
 
+/*
 app.use(
     cookieSession({
         secret: "Anna Karenina",
         maxAge: 1000 * 60 * 60 * 24 * 14,
+    })
+);
+*/
+
+// body parse
+app.use(
+    express.urlencoded({
+        extended: false,
     })
 );
 
@@ -24,25 +33,41 @@ app.get("/petition", function (request, response) {
     response.render("homepage", {
         title: "Petition",
         style: "style.css",
-        body: "homepage.handlebars",
     });
     // response.end("OK");
 });
 
 app.post("/petition", function (request, response) {
-    createSignature(request.body).then((id) => {
-        request.session.signature_id = id;
+    const { first, last, signature } = request.body;
+    console.log("BODY", request.body);
+
+    if (!first || !last) {
+        const error = "Something went wrong, please try again";
+    } else {
+        createSignature(request.body)
+            .then(() => {
+                // id ide u zagradu
+                //   request.session.signature_id = id;
+                response.redirect("/petition/signed");
+            })
+            .catch((error) => {
+                console.log("error", error);
+            });
         response.redirect("/petition/signed");
-    });
+    }
 });
 
 app.get("/petition/signed", function (request, response) {
     console.log("I signed, leave me alone");
+    response.render("signed", {
+        title: "Thank you!",
+        style: "style.css",
+    });
     //response.end("ja sam potpisala");
 });
 
 app.get("/petition/signers", function (request, response) {
-    console.log("List of sinners?");
+    console.log("List of sinners");
     // response.end("lista potpisanih sinera");
 });
 
--- signatures.js
+++ signatures.js
@@ -5,14 +5,16 @@ var db = spicedPg(
     `postgres:${username}:${password}@localhost:5432/${database}`
 );
 
-function createSignature(first, last, signature) {
-    return db.query(
-        "INSERT INTO signatures (first, last, signature) VALUES ($1, $2, $3)",
-        [first, last, signature]
-    );
+function createSignature({ first, last, signature }) {
+    return db
+        .query(
+            "INSERT INTO signatures (first, last, signature) VALUES ($1, $2, $3) RETURNING id",
+            [first, last, signature]
+        )
+        .then((result) => result.rows[0].id);
 }
 
-function getSignatures(first, last, signature) {
+function getSignatures() {
     return db.query("SELECT * FROM signatures").then((result) => result.rows);
 }
 
